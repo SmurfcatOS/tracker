c6cdad7c1904cc5abcc42db1d0d5fc0aabeb6c74
Ved Rane <74463375+Get-DevVed-YT@users.noreply.github.com>
2023-09-26 02:58:33 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+let c = document.getElementById("coords");
+let t = document.getElementById("time");
+let l = document.getElementById("link");
+
+const date = new Date();
+const year = date.getUTCFullYear();
+const month = date.getUTCMonth();
+const day = date.getUTCDate();
+const hours = date.getUTCHours();
+
+const seed = `${year}${month}${day}${hours}`
+const randf = new Math.seedrandom(seed);
+const rand = randf();
+
+const long = (rand * 360 - 180).toFixed(6);
+const lat = (rand * 180 - 90).toFixed(6);
+
+c.innerText = lat + ", " + long;
+t.innerText = date;
+l.innerHTML = `<a href="//www.google.com/maps/place/${lat},${long}">https://www.google.com/maps/place/${lat},${long}</a>`
