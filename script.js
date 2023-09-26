d1e7208a19bc3cecb972ce34007ba28cd272475a
Ved Rane <74463375+Get-DevVed-YT@users.noreply.github.com>
2023-09-26 15:13:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
-let c = document.getElementById("coords");
-let t = document.getElementById("time");
-let l = document.getElementById("link");
+function update(){
+  let c = document.getElementById("coords");
+  let t = document.getElementById("time");
+  let l = document.getElementById("link");
+  
+  let date = new Date();
+  let year = date.getUTCFullYear();
+  let month = date.getUTCMonth();
+  let day = date.getUTCDate();
+  let hours = date.getUTCHours();
+  
+  let seed = `${year}${month}${day}${hours}`
+  let randf = new Math.seedrandom(seed);
+  let rand = randf();
+  
+  let long = (rand * 360 - 180).toFixed(6);
+  let lat = (rand * 180 - 90).toFixed(6);
+  
+  c.innerText = lat + ", " + long;
+  t.innerText = date;
+  l.innerHTML = `<a href="//www.google.com/maps/place/${lat},${long}">https://www.google.com/maps/place/${lat},${long}</a>`
+}
 
-const date = new Date();
-const year = date.getUTCFullYear();
-const month = date.getUTCMonth();
-const day = date.getUTCDate();
-const hours = date.getUTCHours();
-
-const seed = `${year}${month}${day}${hours}`
-const randf = new Math.seedrandom(seed);
-const rand = randf();
-
-const long = (rand * 360 - 180).toFixed(6);
-const lat = (rand * 180 - 90).toFixed(6);
-
-c.innerText = lat + ", " + long;
-t.innerText = date;
-l.innerHTML = `<a href="//www.google.com/maps/place/${lat},${long}">https://www.google.com/maps/place/${lat},${long}</a>`
+setInterval(update, 1000);
