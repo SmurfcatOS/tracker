69aaa25b1f0cd31421dff8c7343e3c7eb5d55ed5
Ved Rane <74463375+Get-DevVed-YT@users.noreply.github.com>
2023-09-26 14:01:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -30,5 +30,5 @@ function update(){
   let lo = (r * 360 - 180).toFixed(6);
   let la = (r * 180 - 90).toFixed(6);
 
-  document.getElementById("tracker2").innerHTML(`<h2>Archive</h2><b>Date: </b><input id="date" type="date"><br><b>Hour: </b><input id="hour" type="number" min="0" max="23"><br><button onclick="update()">Retrieve</button><br><br><b>Coordinates: </b>${la}, ${lo}<br><b>Google Maps Link: </b><a href="//www.google.com/maps/place/${lat},${long}">https://www.google.com/maps/place/${lat},${long}</a>`
+  document.getElementById("tracker2").innerHTML = `<h2>Archive</h2><b>Date: </b><input id="date" type="date"><br><b>Hour: </b><input id="hour" type="number" min="0" max="23"><br><button onclick="update()">Retrieve</button><br><br><b>Coordinates: </b>${la}, ${lo}<br><b>Google Maps Link: </b><a href="//www.google.com/maps/place/${lat},${long}">https://www.google.com/maps/place/${lat},${long}</a>`;
 }
