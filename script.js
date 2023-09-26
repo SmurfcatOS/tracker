eb3822fe09a256168c464bf7e805c25ea89945fb
Ved Rane <74463375+Get-DevVed-YT@users.noreply.github.com>
2023-09-26 13:23:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -17,4 +17,13 @@ const lat = (rand * 180 - 90).toFixed(6);
 
 c.innerText = lat + ", " + long;
 t.innerText = date;
+  
 l.innerHTML = `<a href="//www.google.com/maps/place/${lat},${long}">https://www.google.com/maps/place/${lat},${long}</a>`
+
+function update(){
+  let idate = document.getElementById("date").value;
+  let ihour = document.getElementById("date").value;
+
+  let rf = new Math.seedrandom(`${idate.getYear()}${idate.getMonth()}${idate.getDay()}${hour}`);
+  let r = rf();
+}
